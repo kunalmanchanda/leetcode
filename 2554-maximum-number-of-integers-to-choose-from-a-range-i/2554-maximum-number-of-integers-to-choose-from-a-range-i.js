509a83e4d2a5c44c96ed1009252fd3a67b40c732
Kunal Manchanda <45308206+kunalmanchanda@users.noreply.github.com>
2024-12-11 00:36:37 +0530

    Time: 47 ms (58.65%), Space: 65.9 MB (10.66%) - LeetHub

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} banned
+ * @param {number} n
+ * @param {number} maxSum
+ * @return {number}
+ */
+var maxCount = function(banned, n, maxSum) {
+    let set = new Set();
+
+    banned.forEach(ele => set.add(ele));
+
+    let count = 0;
+    for(let idx = 1; idx <= n; idx++) {
+        if(maxSum - idx >= 0 && !set.has(idx)) {
+            maxSum -= idx;
+            count++;
+        }
+    }
+
+
+    return count;
+};
