a7adfcc9cb936efa5339c36cca7a3b95ef55495b
Kunal Manchanda <45308206+kunalmanchanda@users.noreply.github.com>
2024-12-25 20:08:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (46.06%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -13,19 +13,20 @@ public:
 
         int low = 1;
         int high = n - 2;
+        int mid;
 
         while(low <= high) {
-            int mid = (low + high)/2;
+            mid = (low + high)/2;
 
             if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
                 return mid;
-            } else if(arr[low] <= arr[mid]) {
+            } else if(arr[mid - 1] < arr[mid]) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
 
-        return 0;
+        return mid;
     }
 };
