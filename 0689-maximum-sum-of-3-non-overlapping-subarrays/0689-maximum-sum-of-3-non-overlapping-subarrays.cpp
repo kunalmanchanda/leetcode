89c8b6fbf2c97d6ad5d7bb40bc5fe44210e40a80
Kunal Manchanda <45308206+kunalmanchanda@users.noreply.github.com>
2024-12-28 19:24:37 +0530

    Time: 20 ms (28.46%), Space: 35.4 MB (16.53%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -1,59 +1,66 @@
 class Solution {
-    vector<int> prefix;
-
-    long int helper(int i, int count, vector<int> &arr, int k) {
+    long int helper(int i, int count, vector<int> &arr, int k, vector<vector<int>> &dp) {
         if(count == 0) {
             return 0;
         }
+
         if(i >= arr.size()) {
             return INT_MIN;
         }
 
-        long int take = getSubArraySum(i, i + k - 1) + helper(i + k, count - 1, arr, k);
-        long int notTake = helper(i+1, count, arr, k);
-
-        return max(take, notTake);
-    }
-
-    int getSubArraySum(int i, int j) {
-        if(i == 0) {
-            return prefix[j];
+        if(dp[i][count] != -1) {
+            return dp[i][count];
         }
-        else if(j >= prefix.size()) {
-            return INT_MIN;
-        } else {
-            return prefix[j] - prefix[i-1]; 
-        }
+
+        long int take = arr[i] + helper(i + k, count - 1, arr, k, dp);
+        long int notTake = helper(i+1, count, arr, k, dp);
+        
+        dp[i][count] = max(take, notTake);
+
+        return dp[i][count];
     }
 
     // provides indexes
-    void solve(int i, int count, int k, vector<int> &arr, vector<int> &res) {
+    void solve(int i, int count, int k, vector<int> &arr, vector<int> &res, vector<vector<int>> &dp) {
         if(count == 0) return;
         if(i >= arr.size()) return;
 
-        int takeSum = getSubArraySum(i, i + k - 1) + helper(i + k, count - 1, arr, k);
-        int notTakeSum = helper(i+1, count, arr, k);
+        int takeSum = arr[i] + helper(i + k, count - 1, arr, k, dp);
+        int notTakeSum = helper(i+1, count, arr, k, dp);
 
         if(takeSum >= notTakeSum) {
             res.push_back(i);
-            solve(i + k, count - 1, k, arr, res);
+            solve(i + k, count - 1, k, arr, res, dp);
         } else {
-            solve(i + 1, count, k, arr, res);
+            solve(i + 1, count, k, arr, res, dp);
         }
     }
 public:
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        prefix.resize(n);
-        prefix[0] = nums[0];
+        vector<int> prefix;
 
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
+        int n = nums.size();
+
+        int j = 0, i = 0;
+        int sum = 0;
+
+        while(j < k) {
+            sum += nums[j++];
         }
+
+        prefix.push_back(sum);
+
+        while(j < n) {
+            prefix.push_back(prefix[i] + nums[j++] - nums[i++]);
+        }
+
+        int m = prefix.size();
+
+        vector<vector<int>> dp(m + 1,vector<int>(4, -1));
 
         vector<int> res;
 
-        solve(0, 3, k, nums, res);
+        solve(0, 3, k, prefix, res, dp);
         return res;
     }
 };
