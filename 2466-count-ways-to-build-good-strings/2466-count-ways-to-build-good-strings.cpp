069bc55052fc8e7b2d7c97869d68858e6a6a4df3
Kunal Manchanda <45308206+kunalmanchanda@users.noreply.github.com>
2024-12-30 18:52:44 +0530

    Time: 7 ms (56.16%), Space: 19.3 MB (34.83%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -8,17 +8,13 @@ public:
         if(dp[length] != -1) return dp[length];
 
         int addOne = 0;
-        if(length >= lows && length <= highs) {
-            addOne = 1;
-        }
+        if(length >= lows && length <= highs) addOne = 1;
 
         return dp[length] = (addOne + solve(length + zeros) % mod + solve(length + ones) % mod) % mod;
     }
     int countGoodStrings(int low, int high, int zero, int one) {
         lows = low, highs = high, ones = one, zeros = zero;
-
-        dp.assign(highs + 1, -1);
-        
+        dp.resize(highs + 1, -1);
         return solve(0);
     }
 };
