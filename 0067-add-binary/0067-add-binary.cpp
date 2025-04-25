2d849a3b5e2f6101d67adc53eb9db4fa6e39d042
Kunal Manchanda <45308206+kunalmanchanda@users.noreply.github.com>
2025-04-26 04:03:39 +0530

    Time: 3 ms (26.51%), Space: 11.2 MB (18.94%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -9,18 +9,30 @@ public:
         while(n >= 0 ||  m >= 0 || carry) {
             int sum = (n >= 0 ? a[n] - '0' : 0) + (m >= 0 ? b[m] - '0' : 0) + carry;
 
-            if(sum == 0) {
+            // if(sum == 0) {
+            //     res = '0' + res;
+            //     carry = 0;
+            // } else if(sum == 1) {
+            //     res = '1' + res;
+            //     carry = 0;
+            // } else if(sum == 2) {
+            //    res = '0' + res;
+            //     carry = 1;
+            // } else {
+            //     res = '1' + res;
+            //     carry = 1;
+            // }
+
+            if(sum % 2 == 0) {
                 res = '0' + res;
-                carry = 0;
-            } else if(sum == 1) {
-                res = '1' + res;
-                carry = 0;
-            } else if(sum == 2) {
-               res = '0' + res;
-                carry = 1;
             } else {
                 res = '1' + res;
+            }
+
+            if(sum > 1) {
                 carry = 1;
+            } else {
+                carry = 0;
             }
 
             n--;
