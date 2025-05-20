718b8a690accf2ae374ae3c8c49cb338dab019ee
Kunal Manchanda <45308206+kunalmanchanda@users.noreply.github.com>
2025-05-20 10:00:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    
-    double pow(double x, int n)
-    {
-        if(x == 0)
-        {
-            return 0;
-        }
-        if(n == 0)
-        {    
-            return 1;
-        }
-        
-        if(n == 1)
-        {
+    double myPow(double x, int n) {
+        if(n == 1) {
             return x;
         }
-        
-        if( n  == -1)
-        {
+        if(n == 0) {
+            return 1;
+        }
+        if(n == -1) {
             return 1/x;
         }
-        
-        double res = pow(x, n/2);
-        
-        if(n % 2 == 0)
-        {
+        if(x == 0) {
+            return 0;
+        }
+
+        double res = myPow(x, n/2);
+
+        if(n % 2 == 0) {
             return res * res;
-        }
-        else
-        {
-            if(n < 0)
+        } else {
+            if(n < 1) {
                 return 1/x * res * res;
-            else
-            {
-                return res * res * x;
             }
+            return x * res * res;
         }
-    }
-    
-    double myPow(double x, int n) {
-        return pow(x, n);
     }
 };
